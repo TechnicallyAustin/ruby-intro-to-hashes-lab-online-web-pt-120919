112bfcc42dafe1bd71e794efd77ae7d7f13a3f53
Austin Johnson <technicallyaustin@gmail.com>
2019-12-22 22:42:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,5 +31,5 @@ def monopoly_with_fourth_tier
 	    :names => {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline: {}},
 	    :rent_in_dollars => {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200 }
 }}
-monopoly[:railroads][:names][:reading_railroad] = {"Mortgage_value": "$100"}
+monopoly[:railroads][:names][:reading_railroad] => {"Mortgage_value": "$100"}
 end
